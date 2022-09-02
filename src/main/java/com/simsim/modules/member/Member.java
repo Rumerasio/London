abe96a6d314a82cf94bf850f604b2a14732a0697
Rumerasio <joseph0417@naver.com>
2022-09-02 15:15:28 +0900

    member 연결 및 UI 연결

---
+++ src/main/java/com/simsim/modules/member/Member.java
@@ -0,0 +1,114 @@
+package com.simsim.modules.member;
+
+public class Member {
+
+	private String seq;
+	private String nickname;
+	private String dob;
+	private String gender;
+	private String phoneAgency;
+	private String phoneNum;
+	private String email;
+	private String emailInsert;
+	private String emailDomain;
+	private String id;
+	private String password;
+	private String datetime;
+	private String delNy;
+	private Integer commentNum;
+	private Integer commentLikeNum;
+	//****************************
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhoneAgency() {
+		return phoneAgency;
+	}
+	public void setPhoneAgency(String phoneAgency) {
+		this.phoneAgency = phoneAgency;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmailInsert() {
+		return emailInsert;
+	}
+	public void setEmailInsert(String emailInsert) {
+		this.emailInsert = emailInsert;
+	}
+	public String getEmailDomain() {
+		return emailDomain;
+	}
+	public void setEmailDomain(String emailDomain) {
+		this.emailDomain = emailDomain;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	public String getDelNy() {
+		return delNy;
+	}
+	public void setDelNy(String delNy) {
+		this.delNy = delNy;
+	}
+	public Integer getCommentNum() {
+		return commentNum;
+	}
+	public void setCommentNum(Integer commentNum) {
+		this.commentNum = commentNum;
+	}
+	public Integer getCommentLikeNum() {
+		return commentLikeNum;
+	}
+	public void setCommentLikeNum(Integer commentLikeNum) {
+		this.commentLikeNum = commentLikeNum;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/simsim/modules/member/MemberService.java
@@ -0,0 +1,7 @@
+package com.simsim.modules.member;
+
+import java.util.List;
+
+public interface MemberService {
+	public List<Member> selectList() throws Exception; 
+}
