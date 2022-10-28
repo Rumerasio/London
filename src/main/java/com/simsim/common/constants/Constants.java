b79289101d9a6a52089604a10f8757c20d6a14f9
Joseph <joseph0417@naver.com>
2022-10-28 17:28:08 +0900

    path 관리

--- src/main/java/com/simsim/common/constants/Constants.java
+++ src/main/java/com/simsim/common/constants/Constants.java
@@ -13,6 +13,7 @@ public class Constants {
 
 	public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
 	
+//	public static final String UPLOAD_PATH_PREFIX_EXTERNAL = "D://factory/ws_sts_4151/London/src/main/webapp";
 	public static final String UPLOAD_PATH_PREFIX_EXTERNAL = "D://factory/ws_sts_4151/London/src/main/webapp";
 	public static final String UPLOAD_PATH_PREFIX = "D://factory/ws_sts_4151/London/src/main/webapp/resources/uploaded";
 	public static final String UPLOAD_PATH_PREFIX_FOR_VIEW = "/resources/uploaded";
